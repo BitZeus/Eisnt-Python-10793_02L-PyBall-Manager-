6ea43dfb7b1922d370f917947fa681102c565e39
pfragoso77 <134558690+pfragoso77@users.noreply.github.com>
2023-07-10 23:08:57 +0100

    Função para adicionar equipa selecionada pelo utilizador
    Caixa com botão para criar a equipa do utilizador

--- Pyball_manager.py
+++ Pyball_manager.py
@@ -7,6 +7,7 @@
 #Ficheiro `app.py`:
 import tkinter as tk
 import random
+
 class SimuladorFutebol(tk.Tk):
 
     def __init__(self):
@@ -19,6 +20,28 @@ class SimuladorFutebol(tk.Tk):
     def simular_jogo(self):
         resultado = random.choice(['Vitória', 'Derrota', 'Empate'])
         tk.Label(self, text=f"Resultado do jogo: {resultado}").pack()
+    def criar_equipe():
+        nome_equipe = entry_nome.get()
+    # Salvar o nome da equipe no banco de dados ou em uma variável global para uso posterior
+        print("Equipe criada: ", nome_equipe)
+    # Criar a janela principal
+    window = tk.Tk()
+
+# Criar um rótulo e um campo de entrada para o nome da equipe
+    label_nome = tk.Label(window, text="Nome da Equipe:")
+    label_nome.pack()
+
+    entry_nome = tk.Entry(window)
+    entry_nome.pack()
+
+# Criar um botão para criar a equipe
+    btn_criar_equipe = tk.Button(window, text="Criar Equipe", command=criar_equipe)
+    btn_criar_equipe.pack()
+
+# Iniciar o loop principal da janela
+    window.mainloop()
+
+
 if __name__ == '__main__':
     app = SimuladorFutebol()
     app.mainloop()       
