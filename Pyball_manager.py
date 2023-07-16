2e658528f208a9244854d5f7d776c9adf1b0d3ea
AndreiaMa <andreiasrmar@gmail.com>
2023-07-16 22:57:09 +0100

    update código
    janela de inicialização de jogo

--- Pyball_manager.py
+++ Pyball_manager.py
@@ -43,5 +43,39 @@ class SimuladorFutebol(tk.Tk):
 
 
 if __name__ == '__main__':
-    app = SimuladorFutebol()
-    app.mainloop()       
+    window = tk.Tk()
+    window.title("Inicialização de jogo")
+    window.geometry("500x300")
+
+    label_nome = tk.Label(window, text="Nome da Equipe:")
+    label_nome.pack()
+
+    entry_nome = tk.Entry(window)
+    entry_nome.pack()
+
+    label_nome_treinador = tk.Label(window, text="O teu nome:")
+    label_nome_treinador.pack()
+    entry_nome_treinador = tk.Entry(window)
+    entry_nome_treinador.pack()
+
+    btn_criar_equipe = tk.Button(window, text="Criar Equipe", command=criar_equipe)
+    btn_criar_equipe.pack()
+
+    label_nome = tk.Label(window, text="Equipamento:")
+    label_nome.place(x=70, y=120)
+    radio_var = tk.StringVar(value="op1")
+    #image_path = "camisa_verm.jpg"
+    logo = tk.PhotoImage(file="camisa_verm.gif")
+    tk.Label(window, image=logo).place(x=70, y=150)
+    logo2 = tk.PhotoImage(file="camisa_azul.gif")
+    tk.Label(window, image=logo2).place(x=210, y=150)
+    logo3 = tk.PhotoImage(file="camisa_verde.gif")
+    tk.Label(window, image=logo3).place(x=350, y=150)
+    radio_1 = tk.Radiobutton(window, text="Opção 1", variable=radio_var, value="op1")
+    radio_1.place(x=70, y=240)
+    radio_2 = tk.Radiobutton(window, text="Opção 2", variable=radio_var, value="op2")
+    radio_2.place(x=215, y=240)
+    radio_3 = tk.Radiobutton(window, text="Opção 3", variable=radio_var, value="op3")
+    radio_3.place(x=360, y=240)
+
+    window.mainloop()
